c1029ba2a0c24245bc46173d3a7c909607972add
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2023-02-09 22:10:55 -0300

    ValidaçãoDeCPF

---
+++ index.js
@@ -0,0 +1,54 @@
+class ValidaCPF {
+    constructor (cpfEnviado) {
+        Object.defineProperty(this, 'cpfLimpo', {
+            writable: false, //Não pode escrever nela
+            enumerable: true, // Se FALSE, não vai aparecer quando der um console.log
+            configurable: false,
+            value: cpfEnviado.replace(/\D+/g, '')
+        });
+    }
+
+    ÉSequência() {
+        return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
+    }
+
+    geraNovoCpf() {
+        const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
+        const digito1 = this.geraDigito(cpfSemDigitos);
+        const digito2 = this.geraDigito(cpfSemDigitos + digito1);
+        this.novoCPF = cpfSemDigitos + digito1 + digito2;
+
+    }
+
+
+    geraDigito(cpfSemDigitos) {
+        let total = 0; 
+        let reverso = cpfSemDigitos.length + 1;
+
+        for (let stringNumerica of cpfSemDigitos) {
+            total += reverso * Number(stringNumerica);
+            reverso--;
+        }
+
+        const digito = 11 - (total % 11);
+        return digito <= 9 ? String(digito) : '0'
+    }
+
+    valida() {
+        if (!this.cpfLimpo) return false;
+        if (typeof this.cpfLimpo !== 'string') return false;
+        if (this.cpfLimpo.length !== 11) return 'Digite um total de 11 caracteres';
+        if (this.ÉSequência()) return 'CPF Inválido - Sequência identificada';
+        this.geraNovoCpf();
+
+        return this.novoCPF === this.cpfLimpo;
+    }
+}
+
+const validacpf = new ValidaCPF('070.987.720-03')
+
+if (validacpf.valida()) {
+    console.log('CPF Válido')
+} else {
+    console.log('CPF inválido')
+}
